65cfdaffe7ac2a782ce34b4a839a46d2550c282e
YojiGoto <117175808+yoji->
2022-11-02 14:40:53 +0000

    [Finish]3-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+   config.hosts << "f7f370286b934da0b0f25501715fe0cd.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
